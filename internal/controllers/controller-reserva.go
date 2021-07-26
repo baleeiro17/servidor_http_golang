0294f7dc6c73298538b9c086b71968bf70905d5a
lucas baleeiro <lucasdominato2@gmail.com>
2021-07-25 21:07:17 -0300

    testing array of jsons

--- internal/controllers/controller-reserva.go
+++ internal/controllers/controller-reserva.go
@@ -10,7 +10,7 @@ import (
 func GetReservations(ctx *gin.Context) {
 
 	// get all reservations in database.
-	reserva, err := repositories.GetReserva2()
+	reserva, err := repositories.GetReserva()
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
